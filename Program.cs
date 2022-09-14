e863c922ef274af711f57f3cdce24a3232f74195
Pavel Ponomareve <palych77@mail.ru>
2022-09-15 02:20:35 +0300

    добавил файлы с кодом программы и проект

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//исходный массиы
+string[] str = {"q", "qw", "qwe","w", "w2", "wty","qwerty", "ty", "y","", "y", "y444", "y444"};
+               // {"q", "qw"};
+               //{};
+               //{"q123","q123"};
+
+int j = 0;
+for (int i = 0; i < str.Length; i++){
+   if (str[i].Length >= 3) {
+      str[j] = str[i];
+      j++;
+      
+   }  
+}
+
+string[] str_new = new string[j]; // новый массив, состоящий из элементов исходного, длина которых не меньше 3 символов
+for (int i = 0; i < j; i++){
+   str_new[i] = str[i];  
+   Console.WriteLine(str_new[i]); // вывожу его в этом же цикле
+}
